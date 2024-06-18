310a94ce9f184d6a6fbd532bd5e9e007e9f01275
lukser69 <russergey2001@mail.ru>
2024-06-18 22:48:13 +0300

    feat: components Board & Cell

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,25 @@
+import { FC, Fragment } from "react";
+import { Board } from "../models/Board"
+import CellComponent from "./CellComponent";
+
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+
+  return (
+    <div className="board">
+      {board.cells.map((row, index) => 
+        <Fragment key={index}>
+          {row.map(cell => 
+            <CellComponent cell={cell} key={cell.id}/>
+          )}
+        </Fragment>
+      )}
+    </div>
+  )
+}
+
+export default BoardComponent
