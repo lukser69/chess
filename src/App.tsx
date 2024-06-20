a21b3f9ba908430f064833ff785a54a946ac64b8
lukser69 <russergey2001@mail.ru>
2024-06-20 22:47:13 +0300

    feat: Логика передачи хода

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import {useEffect, useState} from 'react';
 import './App.css'
 import BoardComponent from './components/BoardComponent';
 import { Board } from './models/Board';
+import { Player } from './models/Player';
+import { Colors } from './models/Colors';
 
 const App = () => {
-  const [board, setBoard] = useState(new Board())
+  const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart();
@@ -15,11 +20,21 @@ const App = () => {
     newBoard.initCells();
     newBoard.addFigures();
     setBoard(newBoard);
+    setCurrentPlayer(whitePlayer);
+  }
+
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
   }
   
   return (
     <div className='app'>
-      <BoardComponent board={board} setBoard={setBoard} />
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
     </div>
   );
 };
