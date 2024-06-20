bc4a803fa78b6903f65d738c91d4ebcaa7b8d5a2
lukser69 <russergey2001@mail.ru>
2024-06-20 19:55:30 +0300

    feat: добавлена логика движения фигур

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useEffect, useState } from "react";
 import { Board } from "../models/Board"
 import CellComponent from "./CellComponent";
+import { Cell } from "../models/Cell";
 
 interface BoardProps {
   board: Board;
@@ -8,13 +9,42 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    } else {
+      setSelectedCell(cell);
+    }
+  }
+
+  useEffect(() => {
+    highlightCells();
+  }, [selectedCell])
+
+  function highlightCells() {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
 
   return (
     <div className="board">
       {board.cells.map((row, index) => 
         <Fragment key={index}>
           {row.map(cell => 
-            <CellComponent cell={cell} key={cell.id}/>
+            <CellComponent 
+              cell={cell} 
+              key={cell.id} 
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+              click={click}
+            />
           )}
         </Fragment>
       )}
--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -10,4 +10,21 @@ export class King extends Figure {
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.KING;
   }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+
+    if ((Math.abs(this.cell.x - target.x) === 1
+      || target.x === this.cell.x)
+      && (Math.abs(this.cell.y - target.y) === 1
+        || target.y === this.cell.y)
+      && target.board.getCell(target.x, target.y).isEmpty()) {
+
+      return true;
+    }
+
+    return false;
+  }
 }
